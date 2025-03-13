63022b14b75c1f2282732abb36f5d41dd525b149
Arian Derida <IA9268@PID-PROJECT-IA9268.local>
2025-03-13 15:28:02 +0700

    feat(config): add: global prefix config /api in route

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Set global prefix for all routes
+  app.setGlobalPrefix('api');
+
   await app.listen(3000);
 }
 bootstrap();
