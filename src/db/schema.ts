39d3b2db30f954e460222e168e5f8f5f78c98ad7
Arian Derida <IA9268@PID-PROJECT-IA9268.local>
2025-03-14 16:58:26 +0700

    feat(schema): update: type date and primary key in id

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -12,11 +12,9 @@ export enum TaskLabel {
 
 // src/database/schema.ts
 import { sqliteTable, text, integer } from 'drizzle-orm/sqlite-core';
-import { sql } from 'drizzle-orm';
 
 export const users = sqliteTable('users', {
-  id: integer('id').primaryKey({ autoIncrement: true }),
-  user_id: text('user_id').unique().notNull(),
+  id: integer('id').primaryKey().notNull().unique(),
   name: text('name', { length: 100 }).notNull(),
   email: text('email', { length: 255 }).unique().notNull(),
   password: text('password', { length: 255 }).notNull(),
@@ -24,24 +22,29 @@ export const users = sqliteTable('users', {
     .notNull()
     .default('user'),
   is_active: integer('is_active', { mode: 'boolean' }).default(true),
-  created_at: integer('created_at', { mode: 'timestamp' }).default(
-    sql`CURRENT_TIMESTAMP`,
-  ),
-  updated_at: integer('updated_at', { mode: 'timestamp' }).default(
-    sql`CURRENT_TIMESTAMP`,
-  ),
-  deleted_at: integer('deleted_at', { mode: 'timestamp' }).default(null),
+  created_at: text('created_at')
+    .notNull()
+    .$defaultFn(() => new Date().toISOString()),
+  updated_at: text('updated_at')
+    .notNull()
+    .$defaultFn(() => new Date().toISOString()),
+  deleted_at: text('deleted_at').default(null).$type<Date | null>(),
 });
 
 export const categories = sqliteTable('categories', {
-  id: integer('id').primaryKey({ autoIncrement: true }),
-  category_id: text('category_id').unique().notNull(),
+  id: integer('id').primaryKey().notNull().unique(),
   name: text('name').unique().notNull(),
+  created_at: text('created_at')
+    .notNull()
+    .$defaultFn(() => new Date().toISOString()),
+  updated_at: text('updated_at')
+    .notNull()
+    .$defaultFn(() => new Date().toISOString()),
+  deleted_at: text('deleted_at').default(null).$type<Date | null>(),
 });
 
 export const tasks = sqliteTable('tasks', {
-  id: integer('id').primaryKey({ autoIncrement: true }),
-  task_id: text('task_id').unique().notNull(),
+  id: integer('id').primaryKey().notNull().unique(),
   title: text('title').notNull(),
   description: text('description'),
   user_id: integer('user_id').references(() => users.id, {
@@ -53,11 +56,11 @@ export const tasks = sqliteTable('tasks', {
   status: integer('status').$type<1 | 2 | 3>().notNull().default(1),
   label: integer('label').$type<1 | 2 | 3>().notNull().default(2),
   due_date: integer('due_date', { mode: 'timestamp' }).default(null),
-  created_at: integer('created_at', { mode: 'timestamp' }).default(
-    sql`CURRENT_TIMESTAMP`,
-  ),
-  updated_at: integer('updated_at', { mode: 'timestamp' }).default(
-    sql`CURRENT_TIMESTAMP`,
-  ),
-  deleted_at: integer('deleted_at', { mode: 'timestamp' }).default(null),
+  created_at: text('created_at')
+    .notNull()
+    .$defaultFn(() => new Date().toISOString()),
+  updated_at: text('updated_at')
+    .notNull()
+    .$defaultFn(() => new Date().toISOString()),
+  deleted_at: text('deleted_at').default(null).$type<Date | null>(),
 });
