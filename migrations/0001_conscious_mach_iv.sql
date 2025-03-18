1b9542aa6a6f26a57e64c66b573bcb8a76059f32
Arian Derida <IA9268@PID-PROJECT-IA9268.local>
2025-03-18 12:16:59 +0700

    feat(migration): remove: cleaning unused migration record of drizzle

--- migrations/0001_conscious_mach_iv.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE `categories` ADD `created_at` integer DEFAULT CURRENT_TIMESTAMP;--> statement-breakpoint
-ALTER TABLE `categories` ADD `updated_at` integer DEFAULT CURRENT_TIMESTAMP;--> statement-breakpoint
-ALTER TABLE `categories` ADD `deleted_at` integer DEFAULT 'null';
