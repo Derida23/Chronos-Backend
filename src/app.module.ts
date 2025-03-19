addd44baa0c3c15da5c2e400bc1ae11cb271cd2d
Arian Derida <IA9268@PID-PROJECT-IA9268.local>
2025-03-19 16:27:11 +0700

    feat(profile): add: generate resource profile standarization

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CategoryModule } from './category/category.module';
 import { AuthModule } from './auth/auth.module';
+import { ProfileModule } from './profile/profile.module';
 
 @Module({
-  imports: [AuthModule, CategoryModule],
+  imports: [AuthModule, CategoryModule, ProfileModule],
   controllers: [AppController],
   providers: [AppService],
 })
